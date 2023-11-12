c3c66fa2b54c25f372133c8fb4672647e04e9f2b
Edwards Presume <edwardspresume@gmail.com>
2023-11-12 13:40:15 -0500

    Remove title from Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import LineChart from './components/LineChart';
 export default function Home() {
     return (
         <div className='grid gap-2 p-2 content-center justify-items-center h-[100vh]'>
-            <h1 className='text-2xl font-bold'>Line chart</h1>
-
             <LineChart />
         </div>
     );
