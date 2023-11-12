040d84e45d4d6320b346123959d9e0ac2d5185e1
Edwards Presume <edwardspresume@gmail.com>
2023-11-12 11:54:42 -0500

    removed object with null value from json data and unneeded if statement to check if svgContainer is empty

--- src/app/components/LineChart.tsx
+++ src/app/components/LineChart.tsx
@@ -32,83 +32,77 @@ export default function LineChart() {
 
         // Function to create the chart
         const createChart = (xrp_data) => {
-            // SVG element to which D3 will append the chart
-            const svgElement = chartContainerRef.current;
+            // Create the SVG container inside the div container
+            const svgContainer = d3
+                .select(chartContainerRef.current)
+                .append('svg')
+                .attr(
+                    'width',
+                    chartWidth + chartMargin.left + chartMargin.right
+                )
+                .attr(
+                    'height',
+                    chartHeight + chartMargin.top + chartMargin.bottom
+                )
+                .append('g')
+                .attr(
+                    'transform',
+                    `translate(${chartMargin.left}, ${chartMargin.top})`
+                );
 
-            // Only create the chart once, but update its data/content when needed.
-            if (svgElement && !svgElement.hasChildNodes()) {
-                // Create the SVG container inside the div container
-                const svgContainer = d3
-                    .select(chartContainerRef.current)
-                    .append('svg')
-                    .attr(
-                        'width',
-                        chartWidth + chartMargin.left + chartMargin.right
-                    )
-                    .attr(
-                        'height',
-                        chartHeight + chartMargin.top + chartMargin.bottom
-                    )
-                    .append('g')
-                    .attr(
-                        'transform',
-                        `translate(${chartMargin.left}, ${chartMargin.top})`
-                    );
+            // Define the x-axis scale
+            const xScale = d3
+                .scaleTime()
+                .domain(d3.extent(xrp_data, (d) => d.date))
+                .range([0, chartWidth]);
 
-                // Define the x-axis scale
-                const xScale = d3
-                    .scaleTime()
-                    .domain(d3.extent(xrp_data, (d) => d.date))
-                    .range([0, chartWidth]);
+            // Define the y-axis scale
+            const yScale = d3
+                .scaleLinear()
+                .domain([0.3, d3.max(xrp_data, (d) => d.close)])
+                .range([chartHeight, 0]);
 
-                // Define the y-axis scale
-                const yScale = d3
-                    .scaleLinear()
-                    .domain([0, d3.max(xrp_data, (d) => d.close)])
-                    .range([chartHeight, 0]);
+            // Draw the x-axis on the chart
+            const xAxis = svgContainer
+                .append('g')
+                .attr('transform', `translate(0,${chartHeight})`)
+                .call(d3.axisBottom(xScale))
+                .append('text')
+                .attr('x', chartWidth / 2)
+                .attr('y', 45)
+                .attr('fill', 'white') // Adjust label color to improve visibility
+                .style('text-anchor', 'middle')
+                .style('font-size', '16px')
+                .style('font-weight', 'bold')
+                .text('Date');
 
-                // Draw the x-axis on the chart
-                const xAxis = svgContainer
-                    .append('g')
-                    .attr('transform', `translate(0,${chartHeight})`)
-                    .call(d3.axisBottom(xScale))
-                    .append('text')
-                    .attr('x', chartWidth / 2)
-                    .attr('y', 45)
-                    .attr('fill', 'black') // Adjust label color to improve visibility
-                    .style('text-anchor', 'middle')
-                    .style('font-size', '16px')
-                    .style('font-weight', 'bold')
-                    .text('Date');
+            // Draw the y-axis on the chart
+            const yAxis = svgContainer
+                .append('g')
+                .call(d3.axisLeft(yScale))
+                .append('text')
+                .attr('transform', 'rotate(-90)')
+                .attr('y', -50)
+                .attr('x', -chartHeight / 2)
+                .attr('fill', 'white') // Adjust label color to improve visibility
+                .style('text-anchor', 'middle')
+                .style('font-size', '16px')
+                .style('font-weight', 'bold')
+                .text('Closing price');
 
-                // Draw the y-axis on the chart
-                const yAxis = svgContainer
-                    .append('g')
-                    .call(d3.axisLeft(yScale))
-                    .append('text')
-                    .attr('transform', 'rotate(-90)')
-                    .attr('y', -50)
-                    .attr('x', -chartHeight / 2)
-                    .attr('fill', 'black') // Adjust label color to improve visibility
-                    .style('text-anchor', 'middle')
-                    .style('font-size', '16px')
-                    .style('font-weight', 'bold')
-                    .text('Volume');
+            // Draw the line
+            const line = d3
+                .line()
+                .x((d) => xScale(d.date))
+                .y((d) => yScale(d.close));
 
-                // Draw the line
-                const line = d3
-                    .line()
-                    .x((d) => xScale(d.date))
-                    .y((d) => yScale(d.close));
-
-                svgContainer
-                    .append('path')
-                    .datum(xrp_data)
-                    .attr('fill', 'none')
-                    .attr('stroke', 'steelblue')
-                    .attr('stroke-width', 2)
-                    .attr('d', line);
-            }
+            svgContainer
+                .append('path')
+                .datum(xrp_data)
+                .attr('fill', 'none')
+                .attr('stroke', 'steelblue')
+                .attr('stroke-width', 2)
+                .attr('d', line);
         };
 
         if (!hasChartBeenCreatedRef.current) {
