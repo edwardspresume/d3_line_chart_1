ee03668c8e9755c86aa4d45ec71f06019f18ba78
Edwards Presume <edwardspresume@gmail.com>
2023-11-12 14:13:12 -0500

    Improved typescript

--- src/app/components/LineChart.tsx
+++ src/app/components/LineChart.tsx
@@ -1,113 +1,140 @@
 import * as d3 from 'd3';
 import { useEffect, useRef } from 'react';
 
+type RawDataPoint = {
+    Date: string;
+    Close: string;
+};
+
+type DataPoint = {
+    Date: Date;
+    Close: number;
+};
+
 const chartMargin = { top: 50, right: 50, bottom: 50, left: 70 };
 const chartWidth = 900 - chartMargin.left - chartMargin.right;
-const chartHeight = 900 - chartMargin.top - chartMargin.bottom;
+const chartHeight = 400 - chartMargin.top - chartMargin.bottom;
+
+async function fetchAndProcessData(): Promise<DataPoint[]> {
+    const rawData = await d3.json<RawDataPoint[]>('data/XRP-USD.json');
+
+    if (!rawData) {
+        throw new Error('Data load error: No data returned');
+    }
+
+    const parseDate = d3.timeParse('%Y-%m-%d');
+
+    return rawData
+        .filter(({ Date, Close }) => Date && Close)
+        .map(({ Date, Close }) => ({
+            Date: parseDate(Date)!,
+            Close: parseFloat(Close),
+        }));
+}
 
 export default function LineChart() {
-    const chartContainerRef = useRef(null);
-    const hasChartBeenCreatedRef = useRef(false);
+    const chartContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         // Function to fetch and parse data
         const fetchDataAndCreateChart = async () => {
             try {
-                const data = await d3.json('data/XRP-USD.json');
-                const parseDate = d3.timeParse('%Y-%m-%d');
-                const xrp_data = data.map(({ Date, Close }) => ({
-                    date: parseDate(Date),
-                    close: +Close,
-                }));
+                const xrpData = await fetchAndProcessData();
 
-                if (!hasChartBeenCreatedRef.current) {
-                    createChart(xrp_data);
+                if (xrpData.length > 0 && chartContainerRef.current) {
+                    // Clear the container before creating the chart
+                    d3.select(chartContainerRef.current)
+                        .selectAll('*')
+                        .remove();
 
-                    hasChartBeenCreatedRef.current = true;
+                    // Create the SVG container inside the div container
+                    const svgContainer = d3
+                        .select(chartContainerRef.current)
+                        .append('svg')
+                        .attr(
+                            'width',
+                            chartWidth + chartMargin.left + chartMargin.right
+                        )
+                        .attr(
+                            'height',
+                            chartHeight + chartMargin.top + chartMargin.bottom
+                        )
+                        .append('g')
+                        .attr(
+                            'transform',
+                            `translate(${chartMargin.left}, ${chartMargin.top})`
+                        );
+
+                    // Define the x-axis scale
+                    const xScale = d3
+                        .scaleTime()
+                        .domain(
+                            d3.extent(xrpData, (d) => d.Date) as [Date, Date]
+                        )
+                        .range([0, chartWidth]);
+
+                    // Define the y-axis scale
+                    const yScale = d3
+                        .scaleLinear()
+                        .domain([
+                            0.3,
+                            d3.max(xrpData, (d) => d.Close) as number,
+                        ])
+                        .range([chartHeight, 0]);
+
+                    // Draw the x-axis on the chart
+                    const xAxis = svgContainer
+                        .append('g')
+                        .attr('transform', `translate(0,${chartHeight})`)
+                        .call(d3.axisBottom(xScale))
+                        .append('text')
+                        .attr('x', chartWidth / 2)
+                        .attr('y', 45)
+                        .attr('fill', 'white') // Adjust label color to improve visibility
+                        .style('text-anchor', 'middle')
+                        .style('font-size', '16px')
+                        .style('font-weight', 'bold')
+                        .text('Date');
+
+                    // Draw the y-axis on the chart
+                    const yAxis = svgContainer
+                        .append('g')
+                        .call(d3.axisLeft(yScale))
+                        .append('text')
+                        .attr('transform', 'rotate(-90)')
+                        .attr('y', -50)
+                        .attr('x', -chartHeight / 2)
+                        .attr('fill', 'white') // Adjust label color to improve visibility
+                        .style('text-anchor', 'middle')
+                        .style('font-size', '16px')
+                        .style('font-weight', 'bold')
+                        .text('Closing price');
+
+                    // Draw the line
+                    const line = d3
+                        .line<DataPoint>()
+                        .x((d) => xScale(d.Date))
+                        .y((d) => yScale(d.Close));
+
+                    svgContainer
+                        .append('path')
+                        .datum(xrpData)
+                        .attr('fill', 'none')
+                        .attr('stroke', 'steelblue')
+                        .attr('stroke-width', 2)
+                        .attr('d', line);
                 }
             } catch (error) {
                 console.error('Error fetching the data: ', error);
             }
         };
 
-        // Function to create the chart
-        const createChart = (xrp_data) => {
-            // Create the SVG container inside the div container
-            const svgContainer = d3
-                .select(chartContainerRef.current)
-                .append('svg')
-                .attr(
-                    'width',
-                    chartWidth + chartMargin.left + chartMargin.right
-                )
-                .attr(
-                    'height',
-                    chartHeight + chartMargin.top + chartMargin.bottom
-                )
-                .append('g')
-                .attr(
-                    'transform',
-                    `translate(${chartMargin.left}, ${chartMargin.top})`
-                );
-
-            // Define the x-axis scale
-            const xScale = d3
-                .scaleTime()
-                .domain(d3.extent(xrp_data, (d) => d.date))
-                .range([0, chartWidth]);
-
-            // Define the y-axis scale
-            const yScale = d3
-                .scaleLinear()
-                .domain([0.3, d3.max(xrp_data, (d) => d.close)])
-                .range([chartHeight, 0]);
-
-            // Draw the x-axis on the chart
-            const xAxis = svgContainer
-                .append('g')
-                .attr('transform', `translate(0,${chartHeight})`)
-                .call(d3.axisBottom(xScale))
-                .append('text')
-                .attr('x', chartWidth / 2)
-                .attr('y', 45)
-                .attr('fill', 'white') // Adjust label color to improve visibility
-                .style('text-anchor', 'middle')
-                .style('font-size', '16px')
-                .style('font-weight', 'bold')
-                .text('Date');
-
-            // Draw the y-axis on the chart
-            const yAxis = svgContainer
-                .append('g')
-                .call(d3.axisLeft(yScale))
-                .append('text')
-                .attr('transform', 'rotate(-90)')
-                .attr('y', -50)
-                .attr('x', -chartHeight / 2)
-                .attr('fill', 'white') // Adjust label color to improve visibility
-                .style('text-anchor', 'middle')
-                .style('font-size', '16px')
-                .style('font-weight', 'bold')
-                .text('Closing price');
-
-            // Draw the line
-            const line = d3
-                .line()
-                .x((d) => xScale(d.date))
-                .y((d) => yScale(d.close));
-
-            svgContainer
-                .append('path')
-                .datum(xrp_data)
-                .attr('fill', 'none')
-                .attr('stroke', 'steelblue')
-                .attr('stroke-width', 2)
-                .attr('d', line);
-        };
-
-        if (!hasChartBeenCreatedRef.current) {
-            fetchDataAndCreateChart();
-        }
+        fetchDataAndCreateChart();
     }, []); // Dependency array is empty to ensure effect runs only once
-    return <div ref={chartContainerRef} className='bg-slate-800 rounded'></div>;
+    return (
+        <>
+            <h1 className='text-2xl font-bold'>XRP Closing Price</h1>
+            <div ref={chartContainerRef} className='bg-slate-800 rounded'></div>
+        </>
+    );
 }
